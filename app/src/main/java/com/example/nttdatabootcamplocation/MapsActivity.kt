c8ef23995d664b291db62a8ef7b3f3ef138277c6
Paulo Normando <paulonormando@gmail.com>
2022-07-14 13:36:15 -0300

    Ajuste de localização automática, com Geocode para listagem das informações do endereço atual em um mapa do tipo híbrido

--- app/src/main/java/com/example/nttdatabootcamplocation/MapsActivity.kt
+++ app/src/main/java/com/example/nttdatabootcamplocation/MapsActivity.kt
@@ -1,7 +1,12 @@
 package com.example.nttdatabootcamplocation
 
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.nttdatabootcamplocation.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -12,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(),
     OnMapReadyCallback,
@@ -20,6 +26,11 @@ class MapsActivity : AppCompatActivity(),
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,14 +57,57 @@ class MapsActivity : AppCompatActivity(),
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
         map.getUiSettings().setZoomControlsEnabled(true)
         map.setOnMarkerClickListener(this)
+
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+           ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+           return
+        }
+
+        map.isMyLocationEnabled = true
+
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12.0f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(location:LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+        //markerOptions.icon(BitmapDescriptorFactory.fromBitmap(
+            //BitmapFactory.decodeResource(resources, R.mipmap.ic_user_location)))
+        val titleStr = getAdress(location)
+        markerOptions.title(titleStr)
+
+        map.addMarker(markerOptions)
+    }
+
+    private fun getAdress(latLng: LatLng): String {
+        val geocoder: Geocoder
+        val addresses: List<Address>
+        geocoder = Geocoder(this, Locale.getDefault())
+
+        addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+
+        val address = addresses[0].getAddressLine(0)
+        val city = addresses[0].locality
+        val state = addresses[0].adminArea
+        val country = addresses[0].countryName
+        val postalCode = addresses[0].postalCode
+        return address
     }
 
     override fun onMarkerClick(p0: Marker) = false
